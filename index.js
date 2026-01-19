20adb7dea2897e9cd7cd543d02f33b844903abe4
rishuraj2401 <99868466+rishuraj2401@users.noreply.github.com>
2026-01-19 14:25:10 +0530

    Update and rename app.js to index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from Render Node server" });
+});
+
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
